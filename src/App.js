1a0c601d9cb8792bc102f36a85792cdd4983ebef
NavinNavi19 <navin007.a@gmail.com>
2019-08-12 16:49:00 +0000

    Google Sign In Authentication 2

--- src/App.js
+++ src/App.js
@@ -8,17 +8,42 @@ import HomePage from "./pages/homepage/homepage.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 import ShopPage from "./pages/shop/shop.component";
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route exact path="/signin" component={SignInAndSignUpPage} />
-        <Route path="/shop" component={ShopPage} />
-      </Switch>
-    </div>
-  );
+import { auth } from "./firebase/firebase.utils";
+
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      currentUser: null
+    };
+  }
+
+  unSubscribeFromAuth = null;
+
+  componentDidMount() {
+    this.unSubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({ currentUser: user });
+      console.log(user);
+    });
+  }
+
+  componentWillUnmount() {
+    this.unSubscribeFromAuth = null;
+  }
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/signin" component={SignInAndSignUpPage} />
+          <Route path="/shop" component={ShopPage} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
