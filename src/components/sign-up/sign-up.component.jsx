000028a45573c5ff7025e70ed5fbdfd652ef4f55
NavinNavi19 <navin007.a@gmail.com>
2019-08-12 20:27:27 +0000

    Sign Up With Email and Password

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -60,7 +60,7 @@ class SignUp extends React.Component {
       <div className="sign-up">
         <h2 className="title">I do not have a account</h2>
         <span>Sign up with your email and password</span>
-        <form className="sign-up" onSubmit={this.handleSubmit}>
+        <form onSubmit={this.handleSubmit}>
           <FormInput
             type="text"
             name="displayName"
