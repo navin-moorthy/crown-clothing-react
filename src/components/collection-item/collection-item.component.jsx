5d2dbd0f1b1c23a70c215c6b261a57f3d5ebe9bf
NavinNavi19 <navin007.a@gmail.com>
2019-08-13 15:53:23 +0000

    Add To Cart Styling

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -2,6 +2,8 @@ import React from "react";
 
 import "./collection-item.styles.scss";
 
+import CustomButton from "../custom-button/custom-button.components";
+
 const CollectionItem = ({ id, name, price, imageUrl }) => (
   <div className="collection-item">
     <div className="image" style={{ backgroundImage: `url(${imageUrl})` }} />
@@ -9,6 +11,7 @@ const CollectionItem = ({ id, name, price, imageUrl }) => (
       <span className="name">{name}</span>
       <span className="price">{price}</span>
     </div>
+    <CustomButton inverted>Add to Cart</CustomButton>
   </div>
 );
 
--- src/components/custom-button/custom-button.components.jsx
+++ src/components/custom-button/custom-button.components.jsx
@@ -2,9 +2,16 @@ import React from "react";
 
 import "./custom-button.styles.scss";
 
-const CustomButton = ({ children, isGoogleSignIn, ...otherProps }) => (
+const CustomButton = ({
+  children,
+  isGoogleSignIn,
+  inverted,
+  ...otherProps
+}) => (
   <button
-    className={`${isGoogleSignIn ? "googleSignIn" : ""} custom-button`}
+    className={`${inverted ? "inverted" : ""} ${
+      isGoogleSignIn ? "googleSignIn" : ""
+    } custom-button`}
     {...otherProps}
   >
     {children}
