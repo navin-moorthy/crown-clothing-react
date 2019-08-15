64b65657257b046f8f39ccaa9cdb5ec84a8e1d8b
NavinNavi19 <navin007.a@gmail.com>
2019-08-15 14:36:19 +0000

    Adding Multiple Items To Cart

--- src/pages/shop/shop.data.js
+++ src/pages/shop/shop.data.js
@@ -75,56 +75,56 @@ export const SHOP_DATA = [
     routeName: "sneakers",
     items: [
       {
-        id: 1,
+        id: 10,
         name: "Adidas NMD",
         imageUrl:
           "https://images.unsplash.com/photo-1514989940723-e8e51635b782?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
         price: 220
       },
       {
-        id: 2,
+        id: 11,
         name: "Adidas Yeezy",
         imageUrl:
           "https://images.unsplash.com/photo-1525966222134-fcfa99b8ae77?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
         price: 280
       },
       {
-        id: 3,
+        id: 12,
         name: "Black Converse",
         imageUrl:
           "https://images.unsplash.com/photo-1519810409043-b6d44921bfa2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
         price: 110
       },
       {
-        id: 4,
+        id: 13,
         name: "Nike White AirForce",
         imageUrl:
           "https://images.unsplash.com/photo-1518362165686-c587a1de1003?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
         price: 160
       },
       {
-        id: 5,
+        id: 14,
         name: "Nike Red High Tops",
         imageUrl:
           "https://images.unsplash.com/photo-1508188239917-0fc5f653cb47?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
         price: 160
       },
       {
-        id: 6,
+        id: 15,
         name: "Nike Brown High Tops",
         imageUrl:
           "https://images.unsplash.com/photo-1491553895911-0055eca6402d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
         price: 160
       },
       {
-        id: 7,
+        id: 16,
         name: "Air Jordan Limited",
         imageUrl:
           "https://images.unsplash.com/photo-1460353581641-37baddab0fa2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
         price: 190
       },
       {
-        id: 8,
+        id: 17,
         name: "Timberlands",
         imageUrl:
           "https://images.unsplash.com/photo-1511556532299-8f662fc26c06?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
@@ -138,35 +138,35 @@ export const SHOP_DATA = [
     routeName: "jackets",
     items: [
       {
-        id: 1,
+        id: 18,
         name: "Black Jean Shearling",
         imageUrl:
           "https://images.unsplash.com/photo-1472168087442-46b12008e7c7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
         price: 125
       },
       {
-        id: 2,
+        id: 19,
         name: "Blue Jean Jacket",
         imageUrl:
           "https://images.unsplash.com/photo-1509539662397-116cb90542f1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
         price: 90
       },
       {
-        id: 3,
+        id: 20,
         name: "Grey Jean Jacket",
         imageUrl:
           "https://images.unsplash.com/photo-1559551409-dadc959f76b8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
         price: 90
       },
       {
-        id: 4,
+        id: 21,
         name: "Brown Shearling",
         imageUrl:
           "https://images.unsplash.com/photo-1545594861-3bef43ff2fc8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
         price: 165
       },
       {
-        id: 5,
+        id: 22,
         name: "Tan Trench",
         imageUrl:
           "https://images.unsplash.com/photo-1561748152-7301bfdb8556?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
@@ -180,49 +180,49 @@ export const SHOP_DATA = [
     routeName: "womens",
     items: [
       {
-        id: 1,
+        id: 23,
         name: "Blue Tanktop",
         imageUrl:
           "https://images.unsplash.com/photo-1562801244-b9778719aa99?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
         price: 25
       },
       {
-        id: 2,
+        id: 24,
         name: "Floral Blouse",
         imageUrl:
           "https://images.unsplash.com/photo-1545594262-7df7e6050326?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
         price: 20
       },
       {
-        id: 3,
+        id: 25,
         name: "Floral Dress",
         imageUrl:
           "https://images.unsplash.com/photo-1562262199-f6b3dfb3ec43?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
         price: 80
       },
       {
-        id: 4,
+        id: 26,
         name: "Red Dots Dress",
         imageUrl:
           "https://images.unsplash.com/photo-1564858236937-910aef0b41ca?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
         price: 80
       },
       {
-        id: 5,
+        id: 27,
         name: "Striped Sweater",
         imageUrl:
           "https://images.unsplash.com/photo-1552699749-9d02d0be7dbf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
         price: 45
       },
       {
-        id: 6,
+        id: 28,
         name: "Yellow Track Suit",
         imageUrl:
           "https://images.unsplash.com/photo-1555882860-8a15d7d50a57?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60g",
         price: 135
       },
       {
-        id: 7,
+        id: 29,
         name: "White Blouse",
         imageUrl:
           "https://images.unsplash.com/photo-1552664199-fd31f7431a55?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
@@ -236,42 +236,42 @@ export const SHOP_DATA = [
     routeName: "mens",
     items: [
       {
-        id: 1,
+        id: 30,
         name: "Camo Down Vest",
         imageUrl:
           "https://images.unsplash.com/photo-1505022610485-0249ba5b3675?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
         price: 325
       },
       {
-        id: 2,
+        id: 31,
         name: "Floral T-shirt",
         imageUrl:
           "https://images.unsplash.com/photo-1521341057461-6eb5f40b07ab?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
         price: 20
       },
       {
-        id: 3,
+        id: 32,
         name: "Black & White Longsleeve",
         imageUrl:
           "https://images.unsplash.com/photo-1541418950054-c12804e149d9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
         price: 25
       },
       {
-        id: 4,
+        id: 33,
         name: "Pink T-shirt",
         imageUrl:
           "https://images.unsplash.com/photo-1514222709107-a180c68d72b4?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
         price: 25
       },
       {
-        id: 5,
+        id: 34,
         name: "Jean Long Sleeve",
         imageUrl:
           "https://images.unsplash.com/photo-1515664069236-68a74c369d97?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
         price: 40
       },
       {
-        id: 6,
+        id: 35,
         name: "Burgundy T-shirt",
         imageUrl:
           "https://images.unsplash.com/photo-1553231208-811608243e3f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
