bbb29f049729252574c3c33cf8e2f5ff88f06546
NavinNavi19 <navin007.a@gmail.com>
2019-08-15 19:14:44 +0000

    Remove Items From Cart

--- src/redux/cart/cart.types.js
+++ src/redux/cart/cart.types.js
@@ -1,6 +1,7 @@
 const CartActionTypes = {
   TOGGLE_CART_HIDDEN: "TOGGLE_CART_HIDDEN",
-  ADD_ITEM: "ADD_ITEM"
+  ADD_ITEM: "ADD_ITEM",
+  CLEAR_ITEM_FROM_CARD: "CLEAR_ITEM_FROM_CARD"
 };
 
 export default CartActionTypes;
