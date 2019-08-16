6aba1cea901b8b1de23caeed3001937178511218
NavinNavi19 <navin007.a@gmail.com>
2019-08-16 08:44:04 +0000

    Collection Overview Component

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,23 +1,13 @@
 import React from "react";
-import { connect } from "react-redux";
-import { createStructuredSelector } from "reselect";
 
-import { selectShopCollections } from "../../redux/shop/shop.selectors";
-
-import CollectionPreview from "../../components/collection-preview/collection-preview.component";
+import CollectionsOverview from "../../components/collections-overview/collections-overview.components";
 
 import "./shop.component.scss";
 
 const ShopPage = ({ collections }) => (
   <div className="shop-page">
-    {collections.map(({ id, ...otherCollections }) => (
-      <CollectionPreview key={id} {...otherCollections} />
-    ))}
+    <CollectionsOverview />
   </div>
 );
 
-const mapStateToProps = createStructuredSelector({
-  collections: selectShopCollections
-});
-
-export default connect(mapStateToProps)(ShopPage);
+export default ShopPage;
