fb3bccbc31ec233511089b4805ca03e56c0485ec
NavinNavi19 <navin007.a@gmail.com>
2019-08-23 17:10:28 +0000

    222. Apollo Container

--- graphql-lesson/src/index.js
+++ graphql-lesson/src/index.js
@@ -6,8 +6,7 @@ import { PersistGate } from "redux-persist/integration/react";
 import { ApolloProvider } from "react-apollo";
 import { createHttpLink } from "apollo-link-http";
 import { InMemoryCache } from "apollo-cache-inmemory";
-import { ApolloClient } from "apollo-client";
-import gql from "graphql-tag";
+import { ApolloClient } from "apollo-boost";
 
 import { store, persistor } from "./redux/store";
 
@@ -24,25 +23,6 @@ const client = new ApolloClient({
   link,
   cache
 });
-
-client
-  .query({
-    query: gql`
-      {
-        collections {
-          id
-          title
-          items {
-            id
-            name
-            price
-          }
-        }
-      }
-    `
-  })
-  .then(result => console.log(result))
-  .catch(error => console.error(error));
 
 ReactDOM.render(
   <ApolloProvider client={client}>
