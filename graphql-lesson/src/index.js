82d8152443ff98c1d5395657bf7e5f47762f1337
NavinNavi19 <navin007.a@gmail.com>
2019-08-23 16:27:05 +0000

    SelfHosted GraphQL server with Database from Yihua

--- graphql-lesson/src/index.js
+++ graphql-lesson/src/index.js
@@ -1,21 +1,58 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { ApolloProvider } from "react-apollo";
+import { createHttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { ApolloClient } from "apollo-client";
+import gql from "graphql-tag";
 
-import { store, persistor } from './redux/store';
+import { store, persistor } from "./redux/store";
 
-import './index.css';
-import App from './App';
+import "./index.css";
+import App from "./App";
+
+const link = createHttpLink({
+  uri: "http://35.154.61.191:8080/graphql"
+});
+
+const cache = new InMemoryCache();
+
+const client = new ApolloClient({
+  link,
+  cache
+});
+
+client
+  .query({
+    query: gql`
+      {
+        collections {
+          id
+          title
+          items {
+            id
+            name
+            price
+          }
+        }
+      }
+    `
+  })
+  .then(result => console.log(result))
+  .catch(error => console.error(error));
 
 ReactDOM.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <PersistGate persistor={persistor}>
-        <App />
-      </PersistGate>
-    </BrowserRouter>
-  </Provider>,
-  document.getElementById('root')
+  <ApolloProvider client={client}>
+    <Provider store={store}>
+      <BrowserRouter>
+        <PersistGate persistor={persistor}>
+          <App />
+        </PersistGate>
+      </BrowserRouter>
+    </Provider>
+  </ApolloProvider>,
+  document.getElementById("root")
 );
