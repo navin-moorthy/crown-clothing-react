4c23f4df60a32e9ab054eda505664b95e86fdf44
NavinNavi19 <navin007.a@gmail.com>
2019-08-23 07:10:51 +0000

    Building A Basic Server

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const path = require("path");
+
+if (process.env.NODE_ENV !== "production") require("dotenv").config();
+
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
+const app = express();
+const port = process.env.PORT || 8081;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  });
+}
+
+app.listen(port, error => {
+  if (error) throw error;
+  console.log(`Server running on port ${port}`);
+});
