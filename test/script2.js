844ad0ed14606f8b9564bf8697340e39c31af973
NavinNavi19 <navin007.a@gmail.com>
2019-08-25 07:14:15 +0000

    Asynchronous Tests 2

--- test/script2.js
+++ test/script2.js
@@ -1,28 +1,26 @@
 const fetch = require("node-fetch");
 
-// const getPeoplePromise = fetch => {
-//   return fetch("https://swapi.co/api/people")
-//     .then(res => res.json())
-//     .then(data => {
-//       console.log(data);
-//       return {
-//         count: data.count,
-//         results: data.results
-//       };
-//     });
-// };
+const getPeoplePromise = fetch => {
+  return fetch("https://swapi.co/api/people")
+    .then(res => res.json())
+    .then(data => {
+      return {
+        count: data.count,
+        results: data.results
+      };
+    });
+};
 
 const getPeople = async fetch => {
   const result = await fetch("https://swapi.co/api/people");
   const data = await result.json();
-  console.log(data);
   return {
     count: data.count,
     results: data.results
   };
 };
 
-console.log(getPeople(fetch));
+getPeople(fetch);
 
 module.exports = {
   getPeople,
--- test/script2.test.js
+++ test/script2.test.js
@@ -1,2 +1,18 @@
 const fetch = require("node-fetch");
 const swapi = require("./script2");
+
+it("call swapi to get people", done => {
+  expect.assertions(1);
+  swapi.getPeople(fetch).then(data => {
+    expect(data.count).toEqual(87);
+    done();
+  });
+});
+
+it("call swapi to get people with promise", () => {
+  expect.assertions(2);
+  return swapi.getPeoplePromise(fetch).then(data => {
+    expect(data.count).toEqual(87);
+    expect(data.results.length).toBeGreaterThan(6);
+  });
+});
