87746237b8f3fb515c4cc73bd7a95a85b3d66b2b
NavinNavi19 <navin007.a@gmail.com>
2019-08-25 07:36:01 +0000

    Mocks and Spies

--- test/script2.test.js
+++ test/script2.test.js
@@ -16,3 +16,22 @@ it("call swapi to get people with promise", () => {
     expect(data.results.length).toBeGreaterThan(6);
   });
 });
+
+it("get people return count and results", () => {
+  const mockFetch = jest.fn().mockReturnValue(
+    Promise.resolve({
+      json: () =>
+        Promise.resolve({
+          count: 87,
+          results: [0, 1, 2, 3, 4, 5]
+        })
+    })
+  );
+
+  return swapi.getPeoplePromise(mockFetch).then(data => {
+    expect(mockFetch.mock.calls.length).toBe(1);
+    expect(mockFetch).toBeCalledWith("https://swapi.co/api/people");
+    expect(data.count).toEqual(87);
+    expect(data.results.length).toBeGreaterThan(4);
+  });
+});
