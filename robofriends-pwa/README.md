c215f77ea6009bbc9d7030f0905fe39e8485a7fb
NavinNavi19 <navin007.a@gmail.com>
2019-08-26 05:56:28 +0000

    Removed all the Learning Materials to Seperate Repo

--- robofriends-pwa/README.md
+++
@@ -1,8 +0,0 @@
-# robofriends-pwa
-Udemy tutorial - adding PWA capabilities to a react app
-
-To run the project:
-
-1. Clone this repo
-2. Run `npm install`
-3. Run `npm start`
--- robofriends-pwa/src/components/Card.test.js
+++
@@ -1,9 +0,0 @@
-import { shallow } from "enzyme";
-
-import React from "react";
-import Card from "./Card";
-
-it("expect to render Card component", () => {
-  const cardComponent = shallow(<Card />);
-  expect(cardComponent.debug()).toMatchSnapshot();
-});
